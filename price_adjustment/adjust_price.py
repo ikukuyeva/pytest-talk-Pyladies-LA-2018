ee2c517e45a271f633012e95d5e123e388937fff
Irina Kukuyeva <ikukuyeva@gmail.com>
2021-02-24 12:25:52 -0800

    Add type annotation to fcns

--- price_adjustment/adjust_price.py
+++ price_adjustment/adjust_price.py
@@ -3,7 +3,7 @@
 """
 import math
 
-def price_after_discount(price, discount_percent):
+def price_after_discount(price: float, discount_percent: float) -> float:
     """Function to compute price after discount is applied."""
     if discount_percent > 1:
         raise ValueError("Error: Discount is more than 100%.")
@@ -11,12 +11,12 @@ def price_after_discount(price, discount_percent):
         return price * (1 - discount_percent)
 
 
-def price_after_tax(price, tax_percent):
+def price_after_tax(price: float, tax_percent: float) -> float:
     """Function to compute price after taxes are included."""
     return price * (1 + tax_percent)
 
 
-def price_adjustment(price, adjustment):
+def price_adjustment(price: float, adjustment: float) -> float:
     """More general function for computing price adjustments, than computing
        discounts or price increases separately.
     """
